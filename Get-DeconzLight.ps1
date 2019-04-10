4aa622a84987ec9fbd7739da27fdb65c67adc494
dostse <49457298+dostse@users.noreply.github.com>
2019-04-10 18:58:22 +0200

    Create Get-DeconzLight.ps1

---
+++ Get-DeconzLight.ps1
@@ -0,0 +1,47 @@
+function Get-DeconzLight{
+    [CmdletBinding()]
+    param(
+    [Parameter(Mandatory=$true)]
+    [string]$URI,
+    [Parameter(Mandatory=$true)]
+    [string]$APIKey,
+    [Parameter(Mandatory=$false,ValueFromPipeline=$true)]
+    [string[]]$LightName
+    )
+    BEGIN{
+    
+        $FullURI = "$URI/$APIKey/"
+    
+    }
+    PROCESS{
+
+        if($PSBoundParameters.ContainsKey('LightName')){
+            $Lights = ((Invoke-WebRequest -Uri "$($FullURI)lights").content | ConvertFrom-Json).psobject.properties | Where-Object {$_.value.Name -eq "$LightName"}
+            }
+        else{
+            $Lights = ((Invoke-WebRequest -Uri "$($FullURI)lights").content | ConvertFrom-Json | Sort-Object).psobject.properties
+        }  
+
+
+        foreach($Light in $Lights){
+
+    
+            $Properties = [ordered]@{'Name' = $Light.Value.name
+                                     'Manufacurer' = $Light.Value.manufacturername
+                                     'ModelID' = $Light.Value.modelid
+                                     'UniqueID' = $Light.Value.uniqueid
+                                     'Type' = $Light.Value.type
+                                     'Firmware' = $Light.Value.swversion
+                                     'On' = $Light.Value.State.on
+                                     'Brightness' = $Light.Value.State.bri
+                                     'HasColor' = $Light.Value.Hascolor
+                                     'Reachable' = $Light.Value.state.reachable
+                                     'State' = $Light.Value.state
+            }
+
+            $obj = New-Object -TypeName psobject -Property $Properties
+            Write-Output $obj
+        }
+    }
+    END{}
+}
