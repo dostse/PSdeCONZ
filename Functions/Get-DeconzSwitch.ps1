2356fff39e7d6bd694b7ca9fc6d51123c365ee58
dostse <49457298+dostse@users.noreply.github.com>
2019-04-17 23:05:34 +0200

    Added new function

---
+++ Functions/Get-DeconzSwitch.ps1
@@ -0,0 +1,49 @@
+function Get-DeconzSwitch{
+    [CmdletBinding()]
+    param(
+    [Parameter(Mandatory=$true)]
+    [string]$URI,
+    [Parameter(Mandatory=$true)]
+    [string]$APIKey,
+    [Parameter(Mandatory=$false,ValueFromPipeline=$true)]
+    [string[]]$SwitchName 
+    )
+    BEGIN{
+            
+        $FullURI = "$URI/$APIKey"
+
+    }
+    PROCESS{
+
+        if($PSBoundParameters.ContainsKey('SwitchName')){
+            $Sensors = ((Invoke-WebRequest -Uri "$($FullURI)/sensors").content | ConvertFrom-Json).psobject.properties | Where-Object {$_.value.Type -eq 'ZHASwitch' -and $_.value.Name -eq "$SwitchName"}
+            }
+        else{
+            $Sensors = ((Invoke-WebRequest -Uri "$($FullURI)/sensors").content | ConvertFrom-Json).psobject.properties | Where-Object {$_.value.Type -eq 'ZHASwitch'} | Sort-Object
+        }  
+
+        foreach($Sensor in $Sensors){
+
+            $Group = ((Invoke-WebRequest -Uri "$($FullURI)/groups/$($Sensor.Value.config.group)").content | ConvertFrom-Json).Name
+    
+            $Properties = [ordered]@{'Name' = $Sensor.Value.name
+                                     'Manufacturer' = $Sensor.Value.manufacturername
+                                     'ModelID' = $Sensor.Value.modelid
+                                     'UniqueID' = $Sensor.Value.uniqueid
+                                     'Type' = $Sensor.Value.type
+                                     'Firmware' = $Sensor.Value.swversion
+                                     'Battery' = $Sensor.Value.config.battery
+                                     'On' = $Sensor.Value.config.on
+                                     'Reachable' = $Sensor.Value.config.reachable
+                                     'Group' = $Group
+                                     'LastUpdated' = $Sensor.Value.state.lastupdated
+                                     'Config' = $Sensor.Value.config
+                                     'State' = $Sensor.Value.state
+                                    }
+
+            $obj = New-Object -TypeName psobject -Property $Properties
+            Write-Output $obj
+        }
+    }
+    END{}
+}
